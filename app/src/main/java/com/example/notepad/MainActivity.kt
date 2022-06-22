9919065c41d999a8dcac5197ad1980af1be0edc3
Caleb Langat <langat.caleb95@gmail.com>
2022-06-22 11:59:16 +0300

    Added new activity for NoteList Activity and inserted a list view to match constraints

--- app/src/main/java/com/example/notepad/MainActivity.kt
+++ app/src/main/java/com/example/notepad/MainActivity.kt
@@ -3,11 +3,30 @@ package com.example.notepad
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var spinnerCourses: Spinner
+    private lateinit var textNoteTitle : TextView
+    private lateinit var textNoteText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinnerCourses = findViewById(R.id.spinnerCourses)
+        textNoteTitle = findViewById(R.id.textNoteTitle)
+        textNoteText = findViewById(R.id.textNoteText)
+
+        val dm = DataManager()
+        val adapterCourses = ArrayAdapter<CourseInfo>(this,
+        android.R.layout.simple_spinner_item,dm.courses.values.toList())
+
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerCourses.adapter = adapterCourses
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
