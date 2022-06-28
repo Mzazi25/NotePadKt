08d6c7688c1b9e3955644ff068682058add40ba7
Caleb Langat <langat.caleb95@gmail.com>
2022-06-28 08:58:28 +0300

    Removed listview adapter

--- app/src/main/java/com/example/notepad/NoteListActivity.kt
+++ app/src/main/java/com/example/notepad/NoteListActivity.kt
@@ -34,18 +34,11 @@ class NoteListActivity : AppCompatActivity() {
             val activityIntent = Intent(this,MainActivity::class.java)
             startActivity(activityIntent)
         }
-        listNotes.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, DataManager.notes)
-        listNotes.setOnItemClickListener { parent, view, position, id ->
-            val activityIntent = Intent(this,MainActivity::class.java)
-            activityIntent.putExtra(NOTE_POSITION, position)
-            startActivity(activityIntent)
-        }
 
     }
 
     override fun onResume() {
         super.onResume()
-        (listNotes.adapter as ArrayAdapter<NoteInfo>).notifyDataSetChanged()
     }
 
     override fun onSupportNavigateUp(): Boolean {
